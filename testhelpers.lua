9873c2671199c5e9be43966f3ecaae854e893ff0
Eero af Heurlin <eero.afheurlin@iki.fi>
2014-02-02 17:55:24 +0200

    helpers for syncing

---
+++ testhelpers.lua
@@ -0,0 +1,29 @@
+--[[--
+Helpers for testcases
+--]]--
+
+local testmodule = {}
+testmodule.pulsepin = pio.PB_0
+pio.pin.setdir(pio.OUTPUT, testmodule.pulsepin)
+
+function testmodule.send_syncpulses(num_pulses)
+    -- Sends one long pulse, then num_pulses short ones, then a long pulse, used for syncing with the test script running on the computer
+    testmodule.pulse(5)
+    for pulse=1,num_pulses
+    do
+        -- We get slightly under one ms delay anyway, but this should make it more constant
+        ruuvi.delay_ms(1)
+        testmodule.pulse(2)
+    end
+    ruuvi.delay_ms(1)
+    testmodule.pulse(5)
+end
+
+function testmodule.pulse(msec)
+    -- Pulse the debug pin for given amount of milliseconds
+    pio.pin.sethigh(testmodule.pulsepin)
+    ruuvi.delay_ms(msec)
+    pio.pin.setlow(testmodule.pulsepin)
+end
+
+return testmodule
