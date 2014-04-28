766a7ec7e004f036328e35bc09093b77f48d91a1
Eero af Heurlin <rambo@iki.fi>
2014-04-28 22:58:08 +0300

    change init order and max power

--- software/testcases/five_minute_current_log.py
+++ software/testcases/five_minute_current_log.py
@@ -17,10 +17,10 @@ class five_minute_current_log(rt_testcase):
 
     def setup(self):
         self.hp6632b.display_on(False) # Conserve display
+        self.set_runtime(300) # ~5 minutes
         self.set_log_current_interval(50)
-        self.set_log_voltage_interval(500)
-        self.set_runtime(300) # 5 minutes
-        self.set_power(4100, 1000)
+        self.set_power(4100, 2000)
+        self.set_log_voltage_interval(1500)
 
 if __name__ == '__main__':
     test = five_minute_current_log()
